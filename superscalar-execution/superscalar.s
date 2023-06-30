f3aec019cb447f7d5f32a24ccf0a55be2c8fa4d7
Reece Jones <reece.jones131@gmail.com>
2023-07-01 00:14:06 +1000

    Fixed loop alignment

--- superscalar-execution/superscalar.s
+++ superscalar-execution/superscalar.s
@@ -9,7 +9,7 @@ main:
     mov     %rcx, 314
     mov     %rdx, 159
     mov     %rsi, 265
-.p2align 4      # JCC alignment issue on Skylake (unimportant)
+.p2align 5      # JCC alignment issue on Skylake (unimportant)
 loop:
     add     %rcx, %rcx      # Arithmetic 1
     sub     %rdx, 3         # Arithmetic 2
